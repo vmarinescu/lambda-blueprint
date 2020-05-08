80e953995652ed177313ec6f898cc326b0744130
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-05-08 18:06:22 +0200

    Minor fixes

--- domains/core/test/unit/error-handler.unit.test.ts
+++ domains/core/test/unit/error-handler.unit.test.ts
@@ -3,28 +3,28 @@ import { Error404 } from "../../src/errors/error-404";
 import { handleCreateError, handleGetError } from "../../src/error-handler";
 
 describe("error-handler", () => {
-  it("should return 400 when ValidationError was passed to create-handler", async () => {
+  it("should return 400 when 400-Error was passed to create-handler", async () => {
     expect(handleCreateError(new Error400())).toEqual({
       statusCode: 400,
       body: "",
     });
   });
 
-  it("should return 500 when Error was passed to create-handler", async () => {
+  it("should return 500 when generic Error was passed to create-handler", async () => {
     expect(handleCreateError(new Error())).toEqual({
       statusCode: 500,
       body: "",
     });
   });
 
-  it("should return 404 when NotFoundError was passed to get-handler", async () => {
+  it("should return 404 when 404-Error was passed to get-handler", async () => {
     expect(handleGetError(new Error404())).toEqual({
       statusCode: 404,
       body: "",
     });
   });
 
-  it("should return 500 when Error was passed to get-handler", async () => {
+  it("should return 500 when generic Error was passed to get-handler", async () => {
     expect(handleGetError(new Error())).toEqual({
       statusCode: 500,
       body: "",
--- domains/customer/test/unit/service.unit.test.ts
+++ domains/customer/test/unit/service.unit.test.ts
@@ -9,9 +9,9 @@ const service = new Service();
 describe("service", () => {
   it.todo("should create customer when valid customer has been passed");
 
-  it.todo("should throw ValidationError when invalid customer has been passed");
+  it.todo("should throw 400-Error when invalid customer has been passed");
 
   it.todo("should return customer when customer has been found");
 
-  it.todo("should throw NotFoundError when customer has not been found");
+  it.todo("should throw 404-Error when customer has not been found");
 });
