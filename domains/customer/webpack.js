41bb2e9c1463fbfbe44965146d6b371b65220d57
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-05-14 19:34:14 +0200

    Minor fixes

--- domains/customer/webpack.js
+++ domains/customer/webpack.js
@@ -1,10 +1,10 @@
-const webpackConfig = require("../../webpack.js");
+const webpack = require("../../webpack.js");
 
 module.exports = {
-  ...webpackConfig,
+  ...webpack,
   entry: {
     "create-lambda": "./dist/create-lambda.js",
-    "get-lambda": "./dist/get-lambda.js",
+    "get-lambda":    "./dist/get-lambda.js",
   },
   output: {
     filename: "[name]-bundle.js"
