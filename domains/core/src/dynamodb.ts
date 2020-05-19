8141a8a9dd7b56d46c16e4dbfc506667bbae8263
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-05-19 13:09:12 +0200

    Minor fixes

--- domains/core/src/dynamodb.ts
+++ domains/core/src/dynamodb.ts
@@ -14,6 +14,10 @@ export class CrudRepository<T = any> {
     this.documentClient = new DynamoDB.DocumentClient({});
   }
 
+  /**
+   * Puts a single item by delegating to AWS.DynamoDB.putItem().
+   * @param item
+   */
   async put(item: T): Promise<void> {
     const params: DynamoDB.DocumentClient.PutItemInput = {
       TableName: this.tableName,
@@ -27,6 +31,10 @@ export class CrudRepository<T = any> {
     }
   }
 
+  /**
+   * Gets a single item by the given primary key by delegating to AWS.DynamoDB.getItem().
+   * @param keys
+   */
   async get(keys: Partial<T>): Promise<T> {
     const params: DynamoDB.DocumentClient.GetItemInput = {
       TableName: this.tableName,
@@ -41,6 +49,11 @@ export class CrudRepository<T = any> {
     }
   }
 
+  /**
+   * Updates a single item by the given primary key by delegating to AWS.DynamoDB.updateItem().
+   * @param keys
+   * @param item
+   */
   async update(keys: Partial<T>, item: T): Promise<void> {
     const params: DynamoDB.DocumentClient.UpdateItemInput = {
       TableName: this.tableName,
@@ -57,6 +70,10 @@ export class CrudRepository<T = any> {
     }
   }
 
+  /**
+   * Deletes a single item by the given primary key by delegating to AWS.DynamoDB.deleteItem().
+   * @param keys
+   */
   async delete(keys: Partial<T>): Promise<void> {
     const params: DynamoDB.DocumentClient.DeleteItemInput = {
       TableName: this.tableName,
