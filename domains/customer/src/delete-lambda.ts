51cfc38ffc7f80d7ec48b48bdd72b1dbaffab419
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-05-29 17:12:59 +0200

    Minor fixes

--- domains/customer/src/delete-lambda.ts
+++ domains/customer/src/delete-lambda.ts
@@ -1,8 +1,26 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { CrudRepository } from "@serverless-blueprint/core";
+import { Customer } from "./entities/customer";
+import { Service } from "./service";
+
+const tableName  = process.env.TABLE_NAME!;
+const repository = new CrudRepository<Customer>({ tableName: tableName });
+const service    = new Service(repository);
 
 export async function entrypoint(
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> {
   console.debug("Received customer-event: %s", event);
-  return { statusCode: 204, body: "" };
+
+  const pathParameters = event.pathParameters;
+  if (pathParameters == null) { return { statusCode: 400, body: "" }; }
+
+  try {
+    await service.deleteCustomer(pathParameters["id"]);
+    return { statusCode: 204, body: "" };
+  } catch (reason) {
+    console.debug(reason);
+    return { statusCode: 500, body: "" };
+    // Todo: ...
+  }
 }
--- domains/handover/src/get-lambda.ts
+++ domains/handover/src/get-lambda.ts
@@ -1,8 +1,26 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { CrudRepository } from "@serverless-blueprint/core";
+import { Handover } from "./entities/handover";
+import { Service } from "./service";
+
+const tableName  = process.env.TABLE_NAME!;
+const repository = new CrudRepository<Handover>({ tableName: tableName });
+const service    = new Service(repository);
 
 export async function entrypoint(
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> {
   console.debug("Received handover-event: %s", event);
-  return { statusCode: 200, body: "" };
+
+  const pathParameters = event.pathParameters;
+  if (pathParameters == null) { return { statusCode: 400, body: "" }; }
+
+  try {
+    const handoverDto = await service.getHandover(pathParameters["id"]);
+    return { statusCode: 200, body: JSON.stringify(handoverDto) };
+  } catch (reason) {
+    console.debug(reason);
+    return { statusCode: 500, body: "" };
+    // Todo: ...
+  }
 }
