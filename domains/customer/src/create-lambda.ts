7040fac461f4ebdd7ee8c39e6abd1822d8f4f6a7
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-05-29 15:08:16 +0200

    Minor fixes

--- domains/customer/src/create-lambda.ts
+++ domains/customer/src/create-lambda.ts
@@ -3,6 +3,6 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 export async function entrypoint(
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> {
-  console.debug("Received event: %s", event);
+  console.debug("Received customer-event: %s", event);
   return { statusCode: 201, body: "" };
 }
--- domains/handover/src/delete-lambda.ts
+++ domains/handover/src/delete-lambda.ts
@@ -3,6 +3,6 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 export async function entrypoint(
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> {
-  console.debug("Received event: %s", event);
+  console.debug("Received handover-event: %s", event);
   return { statusCode: 204, body: "" };
 }
