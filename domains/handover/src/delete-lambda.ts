5731dab1b5bcc9dfb9b4091d4246b24e85e7d909
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-05-30 15:05:30 +0200

    Minor fixes

--- domains/handover/src/delete-lambda.ts
+++ domains/handover/src/delete-lambda.ts
@@ -1,4 +1,4 @@
-import { Context, APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { CrudRepository, handleError } from "@serverless-blueprint/core";
 import { Handover } from "./entities/handover";
 import { Service } from "./service";
@@ -6,17 +6,13 @@ import { Service } from "./service";
 const tableName  = process.env.TABLE_NAME || ""; // Todo
 const repository = new CrudRepository<Handover>({ tableName: tableName });
 /**
- * Initialize outside of handler to keep connections alive.
- * @see https://docs.atlas.mongodb.com/best-practices-connecting-to-aws-lambda/
+ * Initialize outside of entrypoint to keep connections alive.
  */
 const service = new Service(repository);
 
 export async function entrypoint(
   event: APIGatewayProxyEvent,
-  context: Context,
 ): Promise<APIGatewayProxyResult> {
-  context.callbackWaitsForEmptyEventLoop = false;
-
   console.debug("Received handover-event: %s", event);
 
   const pathParameters = event.pathParameters;
