56b3a454be81d979c52c790015d014d76ee53b7d
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-05-31 21:20:43 +0200

    Minor fixes

--- domains/handover/handover-stack.ts
+++ domains/handover/handover-stack.ts
@@ -12,6 +12,7 @@ export class HandoverStack extends cdk.Stack {
 
     const env = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_ENV:                            props.nodeEnv,
     };
 
     const dynamoTable = new dynamodb.Table(this, "handovers", {
