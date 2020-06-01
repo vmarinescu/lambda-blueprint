397bbc3443fdc8a971594c42a885addab3bc7b61
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-06-01 19:46:11 +0200

    Minor fixes

--- domains/core/src/ssm.ts
+++ domains/core/src/ssm.ts
@@ -1,20 +1,20 @@
 // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/SSM.html
 
 import * as SSM from "aws-sdk/clients/ssm";
-const ssm = new SSM({});
 
-/**
- * Retrieve names + values of ssm-parameters behind a specific path.
- *
- * @param path e.g. '/qa/{domain}'
- */
+let ssm: SSM;
+
+export const initialize = (options?: SSM.Types.ClientConfiguration): void => {
+  ssm = new SSM(options);
+};
+
 export const getParametersByPath = async (path: string): Promise<Record<string, string>> => {
-  const params: SSM.Types.GetParametersByPathRequest = {
+  const request: SSM.Types.GetParametersByPathRequest = {
     Path:           path,
     WithDecryption: true,
   };
   try {
-    const response = await ssm.getParametersByPath(params).promise();
+    const response = await ssm.getParametersByPath(request).promise();
     const result: Record<string, string> = {};
     // @ts-ignore
     response.Parameters.forEach((parameter) => { result[parameter.Name] = parameter.Value; });
