505e7a6fbf7848fe7d674d54c5102efa316bc053
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-06-02 20:39:01 +0200

    Minor fixes

--- domains/customer/src/utils/service-factory.ts
+++ domains/customer/src/utils/service-factory.ts
@@ -1,10 +1,11 @@
-import { CrudRepository } from "@serverless-blueprint/core";
+import { CrudRepository, getParametersByPath } from "@serverless-blueprint/core";
 import { Customer } from "../entities/customer";
 import { Service } from "./service";
 import { Keys } from "./keys";
 
 export const createService = async (): Promise<Service> => {
-  // Todo: ssm
+  const parameters = await getParametersByPath(`/${process.env[Keys.ENV]}-customer/`);
+  // Todo ...
 
   const tableName  = process.env[Keys.TABLE_NAME] || "";
   const repository = new CrudRepository<Customer>({ tableName: tableName });
--- domains/handover/src/utils/service-factory.ts
+++ domains/handover/src/utils/service-factory.ts
@@ -1,10 +1,11 @@
-import { CrudRepository } from "@serverless-blueprint/core";
+import { CrudRepository, getParametersByPath } from "@serverless-blueprint/core";
 import { Handover } from "../entities/handover";
 import { Service } from "./service";
 import { Keys } from "./keys";
 
 export const createService = async (): Promise<Service> => {
-  // Todo: ssm
+  const parameters = await getParametersByPath(`/${process.env[Keys.ENV]}-handover/`);
+  // Todo ...
 
   const tableName  = process.env[Keys.TABLE_NAME] || "";
   const repository = new CrudRepository<Handover>({ tableName: tableName });
