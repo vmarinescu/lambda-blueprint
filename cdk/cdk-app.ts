40e7f02a98cfcc1a3628c2586e94020fb9990de8
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-06-03 10:01:16 +0200

    Minor fixes

--- cdk/cdk-app.ts
+++ cdk/cdk-app.ts
@@ -1,9 +1,9 @@
 import * as cdk from "@aws-cdk/core";
 import { SharedStackProps } from "./interfaces/shared-stack-props";
 import { DomainStackProps } from "./interfaces/domain-stack-props";
+import { ApigatewayStack } from "./apigateway-stack";
 import { CustomerStack } from "../domains/customer/customer-stack";
 import { HandoverStack } from "../domains/handover/handover-stack";
-import { ApigatewayStack } from "./apigateway-stack";
 
 const app = new cdk.App({});
 
