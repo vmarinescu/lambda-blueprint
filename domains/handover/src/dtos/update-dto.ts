295a51c0e4ba35fc95dfecbecfdc5276f274b67e
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-06-04 17:55:56 +0200

    Minor fixes

--- domains/handover/src/dtos/update-dto.ts
+++ domains/handover/src/dtos/update-dto.ts
@@ -2,10 +2,10 @@ import * as t from "io-ts";
 
 export type  UpdateDto = t.TypeOf<typeof UpdateDto>;
 
-export const UpdateDto = t.partial({
+export const UpdateDto = t.exact(t.partial({
   property1: t.string,
   property2: t.string,
   property3: t.string,
   property4: t.string,
   property5: t.string,
-});
+}));
