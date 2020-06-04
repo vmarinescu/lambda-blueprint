5862272cf613dc357434587f0a91032b1897b160
Dominik Schroettinger <dominik.schroettinger@audi.de>
2020-06-04 20:17:57 +0200

    Minor fixes

--- domains/core/src/utils.ts
+++ domains/core/src/utils.ts
@@ -1,7 +1,7 @@
 export const mergeDeep = (...objects: any[]): any => {
   const isObject = (object: any) => object && typeof object === "object";
 
-  return objects.reduce((prevObject, currObject) => {
+  return objects.reduce((prevObject , currObject) => {
     Object.keys(currObject).forEach((key) => {
       const prevValue = prevObject[key];
       const currValue = currObject[key];
